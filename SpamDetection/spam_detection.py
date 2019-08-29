47f35f74906feac966b2d02900c6efc51b50e54e
NebiyouTen <nebiyoutenager@gmail.com>
2019-08-29 00:12:07 -0600

    Progress work on spam detector

---
+++ SpamDetection/spam_detection.py
@@ -0,0 +1,30 @@
+'''
+    Author: Nebiyou Yismaw
+
+    This is a python code that will naive-bayes algorithm to do a simple spam
+    detection. It uses a dataset from the UCI Machine Learning repository,
+    which can be found https://archive.ics.uci.edu/ml/machine-learning-databases/00228/.
+
+
+'''
+
+import sys
+import pandas as pd
+import numpy as np
+
+
+def main(argv):
+    '''
+        @main
+    '''
+    # let's read the data to a pandas data frame
+    # The data is tab separated, read the label and the sms message
+    data_frame = pd.read_table("SMSSpamCollection", header=None, names=[
+                               'label', 'sms_message'])
+    # convert labels to binary flag (ham=0 and spam=1)
+    data_frame['label'] = data_frame.label.map({"ham": 0, "spam":1})
+    print(data_frame.head(), "\n",data_frame.shape)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ SpamDetection/utils.py
@@ -0,0 +1,8 @@
+'''
+    Author: Nebiyou Yismaw
+
+    This is a python code that will different utility functions for the spam
+    detector. 
+
+
+'''
