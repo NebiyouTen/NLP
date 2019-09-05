f0067e1aff0a670388cdb2290897d8a6405eced8
NebiyouTen <nebiyoutenager@gmail.com>
2019-09-05 00:07:35 -0600

    Adding spam detection using Naive Bayes

--- SpamDetection/spam_detection.py
+++ SpamDetection/spam_detection.py
@@ -13,6 +13,10 @@ import pandas as pd
 import numpy as np
 from utils import *
 import pprint
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection  import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def main(argv):
     '''
@@ -30,8 +34,32 @@ def main(argv):
                  'Call me now.',
                  'Hello, Call hello you tomorrow?']
     bag_of_words_naive = bag_of_words(documents)
-    pprint.pprint(bag_of_words_naive
-    )
+    pprint.pprint(bag_of_words_naive)
+    # now let's use scikit-learn's bag of words
+    # let's split our data to train test
+    X_train, X_test, y_train, y_test = train_test_split(data_frame['sms_message'],
+                                                        data_frame['label'],
+                                                        random_state=1)
+    print('Number of rows in the total set: {}'.format(data_frame.shape[0]))
+    print('Number of rows in the training set: {}'.format(X_train.shape[0]))
+    print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+    # create a countvecotrizer object using our documents list
+    count_vector = CountVectorizer()
+    # Now let's build our traning and testing dataset
+    # Fit the training data and then return the matrix
+    training_data = count_vector.fit_transform(X_train)
+    # Transform testing data and return the matrix. Note we are not fitting
+    # the testing data into the CountVectorizer()
+    testing_data = count_vector.transform(X_test)
+    naive_bayes = MultinomialNB()
+    naive_bayes.fit(training_data, y_train)
+    predictions = naive_bayes.predict(testing_data)
+    print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+    print('Precision score: ', format(precision_score(y_test, predictions)))
+    print('Recall score: ', format(recall_score(y_test, predictions)))
+    print('F1 score: ', format(f1_score(y_test, predictions)))
+
+
 
 
 if __name__ == "__main__":
